ceedd838b2003d4858f64d7714ec4902fd9401ed
Serafin <sfernan5@stevens.edu>
2020-01-27 13:07:02 -0500

    Mean function

--- funcs.cc
+++ funcs.cc
@@ -22,7 +22,7 @@ uint32_t gcd(uint32_t a, uint32_t b);
 uint32_t lcm(uint32_t a, uint32_t b); //Anisha Shin
 double fact(uint32_t n);
 uint64_t sum(uint32_t n);
-double hypot(double a, double b): //Kalani Pigao
+double hypot(double a, double b); //Kalani Pigao
 double diffsq(double a, double b);
 double mean(int a, int b); //Serafin Fernandez
 double mean(int a, int b, int c);
@@ -258,8 +258,10 @@ int main() {
 uint32_t sum(uint32_t a, uint32_t b) { //Nathan Renner
 	return a + b;
 }; 
-
-
+double mean(double a, double b){
+	double m = (a+b)*0.5;
+	return m;
+};
 // ARRAY PROBLEMS
 
 double mean(int x[], int n){ // Nathan Renner
