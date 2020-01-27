c8923b7e0c6339d140ef836cb54da830d425ccb7
sean-mel <smelone@stevens.edu>
2020-01-27 12:29:47 -0500

    name

--- funcs.cc
+++ funcs.cc
@@ -12,7 +12,7 @@ uint32_t sum(uint32_t a, uint32_t b); //Nathan Renner
 uint64_t prod(uint32_t a, uint32_t b);
 uint32_t sumsq(uint32_t a, uint32_t b); //Joshua Hornilla 
 uint32_t countPrimes(uint32_t a, uint32_t b);
-bool isPrime(uint32_t p);
+bool isPrime(uint32_t p); // Sean Melone
 void swap(uint32_t& a, uint32_t& b);
 uint32_t gcd(uint32_t a, uint32_t b);
 uint32_t lcm(uint32_t a, uint32_t b);
@@ -68,7 +68,7 @@ double mean(int x[], int n); // Nathan Renner
 int max(int x[], int n);
 int min(int x[], int n); //Joshua Hornilla 
 double prod(int x[], int n);
-int sum(int x[], int n);
+int sum(int x[], int n); //Sean Melone
 void demean(double x[], int n);
 void normalize(double x[], int n);
 void round(double x[], int n);
